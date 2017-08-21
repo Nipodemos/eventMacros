e5d7a6477f90897d643ccb9a6604b7f8b3991447
nipodemos <a.lanzi@hotmail.com>
2017-08-21 02:16:22 -0300

    multiple updates

--- upAte99Remake/criarMacroTodaNumArquivoSó.bat
+++ upAte99Remake/criarMacroTodaNumArquivoSó.bat
@@ -1,2 +1,3 @@
-copy *.event upAte99Remake.txt
-pause
+@echo off
+DEL eventMacros.txt
+for /r "C:\Users\Alan\Google Drive\Bot\eventMacros\upAte99Remake" %%a in (*.event) do type "%%a" >> "eventMacros.txt"
