57e13e1554efafa785ec1b6c94f1f0d82a34502f
Nipodemos <a.lanzi@hotmail.com>
2017-09-26 02:21:24 -0300

    fix bug on campoDeAprendiz, improvemente on questArruceiro

--- upAte99Remake/criarMacroTodaNumArquivoSó.bat
+++ upAte99Remake/criarMacroTodaNumArquivoSó.bat
@@ -1,3 +1,3 @@
 @echo off
 DEL eventMacros.txt
-for /r "C:\Users\Alan\Google Drive\Bot\eventMacros\upAte99Remake" %%a in (*.event) do type "%%a" >> "eventMacros.txt"
+for %%f in (*.event) do type "%%f" >> "eventMacros.txt"
